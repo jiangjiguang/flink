127ee7363b145c137d251042f79d0884164cedac
jiangjiguang <jiangjiguang@qutoutiao.net>
2020-08-12 19:15:00 +0800

    add log

---
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/libra/sink/DefaultSinkExtraction.java
@@ -0,0 +1,4 @@
+package org.apache.flink.streaming.libra.sink;
+
+public class DefaultSinkExtraction {
+}
---
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/libra/source/DefaultSourceExtraction.java
@@ -0,0 +1,4 @@
+package org.apache.flink.streaming.libra.source;
+
+public class DefaultSourceExtraction {
+}
